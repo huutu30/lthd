d2da4ec164f08fd990fc33e6ab7cf7064df66e78
huu tu <huutu1098@gmail.com>
2024-02-20 22:31:57 +0700

    update

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+
+import { colors } from "./src/global/styles";
+import SignInScreen from "./src/screens/authScreens/SignInScreen";
+import SignUpScreen from "./src/screens/authScreens/SignUpScreen";
+import SignInWelcomeScreen from "./src/screens/authScreens/SignInWelcomeScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import HomeHeader from "./src/components/HomeHeader";
+export default function App(){
+  return(
+    <View style = {styles.container}>
+      
+      <StatusBar
+        barStyle= "light-content"
+        backgroundColor= {colors.statusbar}
+      />
+      {/* <SignInScreen/> */}
+      {/* <SignUpScreen/> */}
+      {/* <SignInWelcomeScreen/> */}
+      <HomeScreen/>
+      {/* <HomeHeader/> */}
+      
+    </ View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container: {flex:1}
+})
+// import React from 'react'
+
+// import {View, Text, StyleSheet,StatusBar} from 'react-native'
+// import  {SignInContextProvider}  from './src/contexts/authContext'
+// import {colors} from './src/global/styles'
+// import RootNavigator from './src/navigation/rootNavigation'
+
+
+
+
+
+
+
+// export default function App(){
+//   return(
+//     <SignInContextProvider>
+//     <View style = {styles.container}>
+//       <StatusBar 
+//         barStyle = "light-content"
+//         backgroundColor = {colors.statusbar}
+//       />
+   
+//         <RootNavigator />
+     
+//     </View>
+//     </SignInContextProvider> 
+  
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   container: {flex:1}
+// })
---
+++ src/global/styles.js
@@ -0,0 +1,18 @@
+
+export const colors = {
+    buttons:"#ff8c52",
+    grey1:'#43484d',
+    grey2:'#5e6977',
+    grey3:'#869392',
+    grey4:'#bdc6cf',
+    grey4:'#e1e8ee',
+    CardComent:'#86939e',
+    Cardbackground: "white",
+    statusbar: '#ff8c52',
+    headerText:"white"
+}
+
+
+export const parameters = {
+    headerHeight :50,
+}
